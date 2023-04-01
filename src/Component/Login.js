315ee48bf42784d26d6c59f4b3e44aa52221533e
priyankapsg <priyaprem1801@gmail.com>
2023-04-01 16:22:52 +0530

    pk commit

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
+      <div> WELCOME </div>
           <div class="mb-3 row">
     <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
     <div class="col-sm-25">
