3c2b2a365f6a120c25419e2d3aa2443b62b25d7c
devadharshinipsg <devadharshinidhanapal.com>
2023-04-01 15:42:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Login from './Component/Login'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Login>
+            
+        </Login>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Login.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+          <div class="mb-3 row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-25">
+      <input type="text" readOnly className="form-control" id="staticEmail" />
+
+    </div>
+  </div>
+  <div class="mb-3 row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-25">
+      <input type="password" className="form-control" id="inputPassword" />
+    </div>
+  </div>
+  <div class="col-auto">
+    <button type="submit" class="btn btn-primary mb-3">Login</button>
+  </div>
+    </div>
+  )
+}
+
+export default Login
